25fe0f6e834d884bff272a5e12b4b152881106e7
Anton Azhhirevych <a.azhhirevych@lampalampa.net>
2024-11-18 23:21:40 +0200

    initialized the project, added basic functionality for displaying the date.

---
+++ src/App.svelte
@@ -0,0 +1,19 @@
+<script>
+  import { onMount } from "svelte";
+
+  let currentDate = '';
+
+  onMount(() => {
+    const date = new Date();
+    currentDate = new Intl.DateTimeFormat('en-US', {
+      weekday: 'long',
+      year: 'numeric',
+      month: 'long',
+      day: 'numeric'
+    }).format(date);
+  });
+</script>
+
+<main>
+  <p>{currentDate}</p>
+</main>
